0f2c87ea146ba0fa4f6a58a70ab68534f131f4b9
tuboat <sethkusperdev@gmail.com>
2018-09-03 21:23:10 -0400

    finished project
    all code works

--- magicPils/magicPils/ViewController.swift
+++ magicPils/magicPils/ViewController.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
 
+    @IBOutlet weak var statePickerButton: UIButton!
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    let states:Array<String> = ["Alaska","Ohio","Texas","New Mexico","Massachusetts"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.red;
-        // Do any additional setup after loading the view, typically from a nib.
+        print(statePickerButton.isHidden);
+        statePicker.dataSource = self;
+        statePicker.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +27,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row];
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerButton.setTitle(states[row], for: .normal)
+        statePicker.isHidden = true;
+        statePickerButton.isHidden = false;
+    }
+    
+    @IBAction func stateButtonPressed(_ sender: Any) {
+        if statePickerButton.isHidden == false{
+            statePickerButton.isHidden = true;
+            statePicker.isHidden = false;
+            
+            
+            
+            
+        }
+    }
+    
 }
 
